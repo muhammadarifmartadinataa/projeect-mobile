e0a9c6b4870e4ae2d2fdbd695bb7240dc7776c74
Muhammad Arif Martadinata <muhammadarifmartadinata@gmail.com>
2024-05-27 16:11:11 +0700

    add : menambahkan fitur dosen

---
+++ lib/model/dosen.dart
@@ -0,0 +1,23 @@
+class Mahasiswa{
+  String? npm;
+  String? nama;
+  String? alamat;
+
+  Mahasiswa({
+    this.npm,
+    this.nama,
+    this.alamat,
+});
+  factory Mahasiswa.fromJson(Map<String, dynamic>json)=> Mahasiswa(
+    npm:json['npm'],
+    nama:json['nama'],
+    alamat:json['alamat'],
+  );
+
+  Map<String, dynamic>toJson() => {
+    'npm':npm,
+    'nama':nama,
+    'alamat':alamat,
+  };
+
+}
